3c0a13996efb8b781b28181bddcc7ec2d54a85d3
Lincerossa90 <marcello.luatti@alkemy.com>
2018-10-09 17:40:47 +0200

    [frontend] aggiunta pagina di prova con getInitialProps

---
+++ packages/frontend/pages/users/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import globalProvider from '../../hoc'
+import * as S from './styles'
+import { Background, Padder, Wrapper } from '../../components/';
+
+import api from '../../api'
+
+
+class Page extends Component {
+
+  static async getInitialProps(){
+
+    const users = await axios.get(api.users).then(x => x.data)
+
+    return {
+      users,
+    }
+
+  }
+
+  render() {
+
+    const { users } = this.props
+
+    console.log({ users })
+
+    return(
+
+      <S.Page>
+          <Background color={{
+            background: "rebeccapurple",
+            text: 'white'
+          }}>
+          <Padder>
+            <Wrapper>
+            {
+              users &&
+              users.length &&
+              users.map(({name, link, avatar_urls}) => (
+                <S.User>
+                  <S.Avar src={avatar_urls[24]} />
+                  <S.Link href={link}>{name}</S.Link>
+                </S.User>
+              ))
+            }
+            </Wrapper>
+          </Padder>
+            
+          </Background>
+        </S.Page>
+
+    )
+  }
+}
+
+export default globalProvider()(Page)
