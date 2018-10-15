26db241aaa1c745ecaa6c6e3ea64ef77d2325abf
Lincerossa90 <marcello.luatti@alkemy.com>
2018-10-15 19:03:11 +0200

    [frontend] progress sulla category page

--- packages/frontend/pages/category/index.js
+++ packages/frontend/pages/category/index.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Link from 'next/link'
+
+
 import globalProvider from '../../hoc'
 import * as S from './styles'
 import { Background, Padder, Wrapper, List, Card } from 'components/lib';
 
 import api from '../../api'
+import { RichText } from 'components/lib/RichText/styles';
 
 
 class Page extends Component {
 
   static async getInitialProps({ query }) {
-
+    
     const category = await axios.get(api.getCategories({ slug: query.category })).then(x => x.data[0])
     const posts = category && await axios.get(api.getPosts({ category: category.id })).then(x => x.data)
+
     return {
       seo: {
-        title: 'category',
+        title: category.name,
       },
       category,
       posts,
@@ -23,33 +28,47 @@ class Page extends Component {
   }
 
   render() {
-    const { category,posts } = this.props
+    const { category, posts } = this.props
     
 
     return(
       <S.Page>
-        <h1>{category.name}</h1>
-        <h2>{category.description}</h2>
+        <Wrapper>
+          <Padder>
+            <S.PageHeader>
+              <RichText>
+                <S.Title>{category.name}</S.Title>
+                <S.Subtitle>{category.description}</S.Subtitle>
+              </RichText>
+            </S.PageHeader>
+          </Padder>
+        
+          
+        </Wrapper>
+        
         {
           posts && posts.length > 0 &&
-          <List items={posts} ListItem={({item}) => {
-            const { title, exerpt, content } = item
-            const props = {
-              media: {
-                url: "http://www.unsplash.it/400/500",
-                title: "titolo dell'immagine",
-                alt: "titolo dell'immagine"
-              },
-              supertitle: category.name,
-              title: title.rendered,
-              subtitle:exerpt &&  exerpt.rendered,
-              content: content.rendered
-            }
-              return <Card {...props} full />
-            }}
-          />
+          <Wrapper>
+            <List items={posts} ListItem={({item}) => {
+              const { title, exerpt, content, slug } = item
+              console.log("item", item)
+              const props = {
+                media: {
+                  url: "http://www.unsplash.it/400/500",
+                  title: "titolo dell'immagine",
+                  alt: "titolo dell'immagine"
+                },
+                supertitle: category.name,
+                title: title.rendered,
+                subtitle:exerpt &&  exerpt.rendered,
+                content: content.rendered
+              }
+                return <Link href={`/${category.name}/${slug}`}><S.A><Card {...props} full /></S.A></Link>
+              }}
+            />
+          </Wrapper>
+         
         }
-      category
       </S.Page>
 
     )
