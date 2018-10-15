dab50a1ee0190145c372ea4853b77530b73db345
Lincerossa90 <marcello.luatti@alkemy.com>
2018-10-15 16:30:57 +0200

    [frontend] api (versione beta... da ottimizzare)

--- packages/frontend/api/index.js
+++ packages/frontend/api/index.js
@@ -1,11 +1,18 @@
 const main = `${process.env.WEBSITE}/wp-json/wp/v2`
 
 const api = {
-  main,
   users: `${main}/users`,
+  getUser: id => `${main}/users/${id}`,
+
+  posts: `${main}/posts`,
+  getPost: id => `${main}/posts/${id}`,
+  getPostsByCategoryId: categoryId => `${main}/posts/?categories=${categoryId}`,
+  categories: `${main}/categories`,
+  getCategory: id => `${main}/categories/${id}`,
+  getCategoryBySlug: slug => `${main}/categories/?slug=${slug}`,
+
   media: `${main}/media`,
-  authors: `${main}/users`,
-  getAuthor: id =>`${main}/users/${id}`,
 }
+
 
 export default api
